19aba4963df178ecc94d89650ec5e758ff8f7194
Jinzakk <denis.zhujov@gmail.com>
2018-05-13 17:05:36 +0500

    update code

--- app/helpers/current-moment.js
+++ app/helpers/current-moment.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Helper.extend({
   compute([broadcastsArray, index]) {
-    const minDate = broadcastsArray.reduce((prev, current) => (prev.date < current.date) ? prev : current).date;
+    const minDate = broadcastsArray.reduce((prev, current) => (prev.date < current.date) ? prev : current);
 
-    return broadcastsArray[index].date === minDate ? 'active' : '';
+    return broadcastsArray[index].date === minDate.date ? 'active' : '';
   }
 });
