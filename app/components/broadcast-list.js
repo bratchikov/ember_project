c3b92e600fa61e5594da841e5589ec8a57102e51
DZhunev <dzhunev@ics.perm.ru>
2018-06-02 14:43:23 +0500

    created rating array

--- app/components/broadcast-list.js
+++ app/components/broadcast-list.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   // attributeBindings: ['tbodyStyle:style'],
   // tbodyStyle: 'background-color: blue; font-size: .9em;',
   broadcasts: null,
+  activeRatingArray: [],
 
   broadcastManager: Ember.inject.service(),
 
@@ -14,10 +15,36 @@ export default Ember.Component.extend({
     return [`startTime:asc`];
   }),
 
+  getActiveRating: Ember.computed('activeRatingArray', function() {
+    const rating = this.get('activeRatingArray');
+
+    console.log(rating)
+  }),
+
+  didRender() {
+    this._super(...arguments);
+    const activeRatingArray = this.get('activeRatingArray');
+
+    activeRatingArray.map(index => {
+      Ember.$(`.ui.rating.${index}`).find('.ui.rating').find('.icon').addClass('active');
+    });
+  },
+
   actions: {
     toggleModalWindow(broadcastName, broadcastInfo) {
       this.get('broadcastManager').addNameAndInfo(broadcastName, broadcastInfo);
       Ember.$('.ui.profile.modal').modal('show');
     },
+    activeRating(index) {
+      const someIndex = item => item == index;
+      const activeRatingArray = this.get('activeRatingArray');
+
+      if(!activeRatingArray.some(someIndex)) {
+        activeRatingArray.push(index);
+      } else {
+        const getIndexRemovingItem = activeRatingArray.indexOf(index);
+        activeRatingArray.splice(getIndexRemovingItem, 1);
+      }
+    }
   },
 });
