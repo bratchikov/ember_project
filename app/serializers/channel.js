495ba274219d0fd2aaacf29a8a04b518dfbc2a9c
Bratchikov Igor <igor@bratchikovy.ru>
2018-06-19 18:05:24 +0500

    Update channel.js

--- app/serializers/channel.js
+++ app/serializers/channel.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
+  // ?
   // normalizeResponse(store, primaryModelClass, payload, id, requestType) {
   //   payload = {channels: payload};
 
