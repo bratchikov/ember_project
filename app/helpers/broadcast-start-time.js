14b9d2a213a2e5628e0e716ebf853cb530128e88
Bratchikov Igor <igor@bratchikovy.ru>
2018-06-19 17:55:47 +0500

    Update broadcast-start-time.js

--- app/helpers/broadcast-start-time.js
+++ app/helpers/broadcast-start-time.js
@@ -4,6 +4,7 @@ export default Ember.Helper.extend({
   compute([getProgramOfBroadcasts, startTime, id, index]) {
     let activeClass = '';
 
+    // Довольно небезопасный код. Очень легко в таком варианте получить undefined при обращении по индексу, нужна обработка ошибок.
     if((getProgramOfBroadcasts[index].id != 0 && getProgramOfBroadcasts[index-1].endProgramTime == startTime) || 
       (getProgramOfBroadcasts[index].id == 0 && id == 0)) {
       activeClass = 'active';
