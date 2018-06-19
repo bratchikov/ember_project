158bb5b38126afa2d50f5df92ecfa417b4475166
Bratchikov Igor <igor@bratchikovy.ru>
2018-06-19 18:06:47 +0500

    Update broadcast-manager.js

--- app/services/broadcast-manager.js
+++ app/services/broadcast-manager.js
@@ -5,6 +5,7 @@ export default Ember.Service.extend({
   broadcastInfo: null,
   minDate: null,
 
+  // Лучше было назвать setNameAndInfo. Ну и логично было бы в пару иметь метод getNameAndInfo, наверное.
   addNameAndInfo(name, info) {
     this.set('broadcastName', name);
     this.set('broadcastInfo', info);
